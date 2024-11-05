624c1421d87143630e926571b1e4155261e538bf
Eldar398 <superwatermelony@gmail.com>
2024-11-04 20:50:36 -0600

    Movement code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -19,14 +19,22 @@ public class Teleop extends LinearOpMode{
         //Right Motor
         DcMotor r_motor = hardwareMap.get(DcMotor.class,"r_motor");
         //Arm Motor
-        DcMotor a_motor = hardwareMap.get(DcMotor.class,"A_motor"):
+        DcMotor a_motor = hardwareMap.get(DcMotor.class,"a_motor");
+        //Pivot Motors
+        DcMotor lp_motor = hardwareMap.get(DcMotor.class,"lp_motor");
+        DcMotor rp_motor = hardwareMap.get(DcMotor.class,"rp_motor")
         //Code to reverse Motor direction
         //l_motor.setDirection(DcMotorSimple.Direction.REVERSE);
         //r_motor.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        //lp_motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        //rp_motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        //Postion for linear extension
         int p1 = 0;
         int p2 = 0;
-
+        //Postion for Pivot
+        int pos_up = 0;
+        int pos_stright = 0;
+        int pos_down = 0:
         waitForStart();
         while (opModeIsActive()) {
             //setting joystick values
@@ -38,7 +46,7 @@ public class Teleop extends LinearOpMode{
             l_motor.setPower(ly + rx);
             r_motor.setPower(ly - rx);
 
-
+            //Arm extension
             if (gamepad1.a) {
                 a_motor.setTargetPosition(p1);
                 a_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -52,6 +60,25 @@ public class Teleop extends LinearOpMode{
                 else{
                     a_motor.setPower(0);
                 }
+            //Pivot?
+            if (gamepad1.dpad_up){
+                lp_motor.setTargetPosition(pos_up);
+                lp_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                lp_motor.setPower(1);
+            }
+            else if (gamepad1.dpad_left){
+                lp_motor.setTargetPosition(pos_stright);
+                lp_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                lp_motor.setPower(1);
+            }
+            else if (gamepad1.dpad_down){
+                lp_motor.setTargetPosition(pos_down);
+                lp_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                lp_motor.setPower(-1);
+            }
+            else{
+                lp_motor.setPower(0);
+                rp_motor.setPower(0);
             }
         }
     }
